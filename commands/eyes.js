41476934ab1347b27d7dff45240e68f9c24824ad
mortmortmort <88593986+mortmortmort@users.noreply.github.com>
2021-09-20 23:57:47 -0400

    Update eyes.js

--- commands/eyes.js
+++ commands/eyes.js
@@ -4,29 +4,46 @@ const countdown = require('moment-countdown');
 const moment = require('moment');
 const config = require("../config.json");
 
+const eyesData = {
+    "Kourmonen": { "emoji": "johnbob" },
+    "Lamaa": { "emoji": "pat" },
+    "Y-MPWL": { "emoji": "really" },
+    "Camal": { "emoji": "catgun" }
+};
 
-exports.run = async (client, message, args) => {
-    
+exports.run = async (client, message, args) => {    
     var messageSanitized = message.content.toLowerCase();
     var command = message.content.startsWith(`${config.prefix}eyes`);
     var arg = messageSanitized.replace(`${config.prefix}eyes `, '');
+    
     const kourm = message.guild.emojis.cache.find(emoji => emoji.name === 'johnbob');
     const lamaa = message.guild.emojis.cache.find(emoji => emoji.name === 'pat');
     const ymp = message.guild.emojis.cache.find(emoji => emoji.name === 'really');
     const camal = message.guild.emojis.cache.find(emoji => emoji.name === 'catgun');
-    message.channel.send(`@everyone \n**Eyes Monitoring Started.**\n\n
-    **Kourmonen:** 
-    **Lamaa:** 
-    **Y-MP:** 
-    **Camal:** \n
     
-    **React with:**
+    var messageText = `@everyone \n**Eyes Monitoring Started.**\n\n`;
+    
+    for (var system in eyesData) {
+        messageText += "**" + system + "**\n";
+    }
+   
+    messageText += `\n
+    
+    **React with:**`;
+
+    /*
+    for (var system in eyesData) {
+        messageText += "**" + system + "**\n";
+    }
     ${kourm} = Kourmonen\n
     ${lamaa} = Lamaa\n
     ${ymp} = Y-MP\n
     ${camal} = Camal\n
     
-    `).then(sentEmbed => {
+    `;*/
+
+    
+    message.channel.send(messageText).then(sentEmbed => {
         sentEmbed.react(kourm)
         sentEmbed.react(lamaa)
         sentEmbed.react(ymp)
