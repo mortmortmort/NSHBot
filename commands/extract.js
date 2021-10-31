1f72c415d3916d77094726cae29b644efadd1b1f
Mortis <mortmort@mail.com>
2021-10-31 03:51:00 +0000

    Migrate 'jr' to typescript

--- commands/extract.js
+++ commands/extract.js
@@ -2,9 +2,9 @@ const Permissions = require("../types/permissiontypes.js");
 const Processor = require("../processors/legacycommands");
 
 exports.getPermissions = () => {
-    return { User: Permissions.UserPermissions.All, Channel: Permissions.ChannelPermissions.All };
+    return { User: Permissions.UserPermissions.Public, Channel: Permissions.ChannelPermissions.All };
 };
 
 exports.run = async (client, message, args) => {
-  return Processor.LegacyCommands.extract(client, message, args);
+    return Processor.LegacyCommands.extract(client, message, args);
 };
--- processors/legacycommands.ts
+++ processors/legacycommands.ts
@@ -6,9 +6,24 @@ import { BotClient } from "../botclient";
 import { DebugLevel, debugLevelFromString } from "../types/debugtypes";
 
 const rp = require("request-promise");
+const fetch = require('node-fetch');
+
+async function getSystemIdFromSystemName(systemName: string): Promise<number> {
+    let data = await fetch(`https://esi.evetech.net/latest/search/?categories=solar_system&datasource=tranquility&language=en-us&search=${systemName}&strict=false`);
+    let json = await data.json();
+
+    return json.solar_system;
+}
+
+async function getSystemNameFromId(systemId: number): Promise<string> {
+    let data = await fetch(`https://esi.evetech.net/latest/universe/systems/${systemId}/?datasource=tranquility&language=en-us`);
+    let json = await data.json();
+    return json.name;
+}
 
 export class LegacyCommands {
     static async extract(client: BotClient, message: Message, args: string[]): Promise<void> {
+        client.debug.logTrace(`LegacyCommands::extract() invoked. args = '${args}'`);
 
         async function extractor() {
             const request = {
@@ -61,5 +76,42 @@ export class LegacyCommands {
             profit.toLocaleString()
         );
         message.channel.send({ embeds: [ embed ] });
+    }
+
+    static async jr(client: BotClient, message: Message, args: string[]): Promise<void> {
+        client.debug.logTrace(`LegacyCommands::jr() invoked. args = '${args}'`);
+
+        let sArray = args;
+
+        let fID;
+        let tID;
+
+        //Get From System's System ID
+        if (sArray.length === 3) {
+            fID = await getSystemIdFromSystemName(sArray[1]);
+            tID = await getSystemIdFromSystemName(sArray[2]);
+        } else {
+            fID = await getSystemIdFromSystemName(sArray[0]);
+            tID = await getSystemIdFromSystemName(sArray[1]);
+        }
+
+        //Get From System's System Name 
+        let fName = await getSystemNameFromId(fID);
+
+        //Get To System's System Name
+        let tName = await getSystemNameFromId(tID);
+
+        //Get Shortest Jump Route via Gates 
+        
+        //Get Thera from "From" System and remove non K Space Entrances
+        if (sArray.length == 2) {
+            message.reply(`https://evemaps.dotlan.net/jump/Archon,544,S/${fName}:${tName}`)
+        } else if (sArray.length === 3 && sArray[0] === 'blops'){
+            message.reply(`https://evemaps.dotlan.net/jump/Panther,544,S/${fName}:${tName}`)
+        } else if(sArray.length === 3 && sArray[0] === 'jf'){
+            message.reply(`https://evemaps.dotlan.net/jump/Nomad,544,S/${fName}:${tName}`)
+        } else {
+            message.reply("I don't recognize the arguements you provided. The command is either: !jr <systemFrom> <systemTo> **OR** !jr blops <systemFrom> <systemTo> **OR** !jr jf <systemFrom> <systemTo>")
+        }
     }
 };
